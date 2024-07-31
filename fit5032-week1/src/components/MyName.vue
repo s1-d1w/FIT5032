2baa42bcf3b6690c05d3de9020b55a114dad47ea
Diwen Si <sdw@dyn-118-138-73-131.its.monash.edu.au>
2024-07-31 15:40:23 +1000

    update

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <p>{{ name }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Diwen Si'
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  p {
+    font-size: 40px;
+    color: #42b983;
+  }
+  </style>
