d501e9150dafe728862776cb25d64e4b8938598c
s1-d1w <diwen.lu.s@gmail.com>
2024-08-02 23:18:58 +1000

    update of week 2

--- dsi-library/src/App.vue
+++ dsi-library/src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import MyName from './components/MyName.vue'
+import JSON from './components/JSON.vue';
 </script>
 
 <template>
@@ -9,13 +7,11 @@ import MyName from './components/MyName.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <MyName />
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <JSON />
   </main>
 </template>
 
